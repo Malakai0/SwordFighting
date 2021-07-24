c763c26fb00944f0c099ce0eafe55d6b2ac14259
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-07-24 19:13:07 +0100

    Removed unnecessary prints.

--- src/Server/Game/Modules/ServerMove.lua
+++ src/Server/Game/Modules/ServerMove.lua
@@ -3,7 +3,6 @@ local Knit = require(game:GetService("ReplicatedStorage").Knit)
 
 function ServerMove.NormalAttack(HurtFunc, Player, Recipient, Damage)
     HurtFunc();
-    print('Server damage: ' .. Damage);
 end
 
 return ServerMove
